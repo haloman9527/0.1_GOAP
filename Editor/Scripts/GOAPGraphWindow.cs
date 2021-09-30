cbe643cf879b2b48fd5263f859dd78f610fbeb63
HalfLobsterMan <766439226@qq.com>
2021-09-30 18:52:15 +0800

    update

--- Editor/Scripts/GOAPGraphWindow.cs
+++ Editor/Scripts/GOAPGraphWindow.cs
@@ -93,12 +93,12 @@ namespace CZToolKit.GOAP.Editors
 
         protected override IEnumerable<Type> GetNodeTypes()
         {
-            foreach (var type in Utility_Reflection.GetChildrenTypes<GOAPAction>())
+            foreach (var type in Utility_Reflection.GetChildTypes<GOAPAction>())
             {
                 if (type.IsAbstract) continue;
                 yield return type;
             }
-            foreach (var type in Utility_Reflection.GetChildrenTypes<GOAPActionEvtNode>())
+            foreach (var type in Utility_Reflection.GetChildTypes<GOAPActionEvtNode>())
             {
                 if (type.IsAbstract) continue;
                 yield return type;
